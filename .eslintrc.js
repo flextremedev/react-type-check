86070bbc4e70b5a0e74df8c17ffd99a9ed54d835
Pascal Wegner <p.wegner1992@gmail.com>
2020-05-30 15:34:37 +0200

    Prettier setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,16 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true,
-    },
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-    rules: {
-        "no-console": 2,
-    },
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'no-console': 2,
+  },
 };
