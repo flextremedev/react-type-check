c5abd0ffde15011da16ec7d0fdbf85161baebc01
flextremedev <p.wegner1992@gmail.com>
2020-06-02 17:47:44 +0200

    Coverage threshold

--- testing/jest.config.js
+++ testing/jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
   preset: 'ts-jest',
   testEnvironment: 'node',
   globals: {
