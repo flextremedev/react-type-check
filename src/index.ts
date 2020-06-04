07e4fa820dd9bda090967a8ea5578c86ae791916
flextremedev <p.wegner1992@gmail.com>
2020-06-04 19:25:15 +0200

    Facade

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { isClassComponent } from './is-class-component/isClassComponent';
+export { isFunctionComponent } from './is-function-component/isFunctionComponent';
+export { isReactComponent } from './is-react-component/isReactComponent';
